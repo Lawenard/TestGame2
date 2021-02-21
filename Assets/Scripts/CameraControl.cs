d9ba5b8818970b654917549bcf54782fd2e49c5d
Lawenard <max.v.yur@gmail.com>
2021-02-21 17:00:55 +0200

    First push

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField] private Joystick joystick;
+    [SerializeField] private float speed;
+
+    [SerializeField] private Transform xPivot;
+    [SerializeField] private Transform yPivot;
+
+    void Update()
+    {
+        xPivot.Rotate(Vector3.up * (joystick.Direction.x * speed * Time.deltaTime));
+        yPivot.Rotate(Vector3.left * (joystick.Direction.y * speed * Time.deltaTime));
+    }
+}
---
+++ Assets/Scripts/Joystick.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Joystick : EventTrigger
+{
+    [SerializeField] private RectTransform stick;
+
+    private RectTransform joystick;
+    private PointerEventData data;
+
+    public Vector2 Direction { get; private set; }
+
+    private void Awake()
+    {
+        joystick = GetComponent<RectTransform>();
+        Direction = Vector2.zero;
+    }
+
+    public override void OnPointerDown(PointerEventData data)
+    {
+        this.data = data;
+    }
+
+    public override void OnPointerUp(PointerEventData data)
+    {
+        this.data = null;
+        stick.anchoredPosition = Vector2.zero;
+        Direction = Vector2.zero;
+    }
+
+    private void Update()
+    {
+        if (data != null)
+        {
+            Vector2 diff, direction;
+            diff = joystick.InverseTransformPoint(data.position - joystick.sizeDelta / 4);
+            direction = Vector2.ClampMagnitude(diff, joystick.sizeDelta.x / 2);
+            stick.anchoredPosition = direction;
+            this.Direction = direction.normalized;
+        }
+    }
+}
